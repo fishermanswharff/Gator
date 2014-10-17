8fec598a3a74afc505c7f0304a5ed00794553403
fishermanswharff <fishermanswharff@github.com>
2014-10-17 16:54:23 -0400

    environment variables, configuring vars in dev.rb

--- config/initializers/environment_variables.rb
+++ config/initializers/environment_variables.rb
@@ -1,9 +1,9 @@
 module EnvironmentVariablesExample
   class Application < Rails::Application
     config.before_configuration do
-      aws_file = Rails.root.join("config", 'aws.yml').to_s
-      if File.exists?(aws_file)
-        YAML.load_file(aws_file)[Rails.env].each do |key, value|
+      env_file = Rails.root.join("config", 'env.yml').to_s
+      if File.exists?(env_file)
+        YAML.load_file(env_file)[Rails.env].each do |key, value|
           ENV[key.to_s] = value
         end
       end
