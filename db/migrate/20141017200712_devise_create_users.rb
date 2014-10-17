818f3b06bb24f033877dbb139c2d5425757707ae
fishermanswharff <fishermanswharff@github.com>
2014-10-17 16:09:01 -0400

    ran user migration

--- db/migrate/20141017200712_devise_create_users.rb
+++ db/migrate/20141017200712_devise_create_users.rb
@@ -5,6 +5,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      t.string :username
+      t.string :firstname
+      t.string :lastname
+      
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
