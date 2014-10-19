c1de557640c82b9526672cd37bc8254df0d63bcb
fishermanswharff <fishermanswharff@github.com>
2014-10-19 13:57:11 -0400

    adding clients to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,4 +74,27 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # configure the amazon server creds
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :s3_credentials => {
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+  
+  # config twitter creds
+  TwitterClient = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+    config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+    config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+    config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+  end
+
+  InstagramClient = Instagram.configure do |config|
+    config.client_id = ENV["INSTAGRAM_CLIENT_ID"]
+    config.client_secret = ENV["INSTAGRAM_CLIENT_SECRET"]
+  end
 end
