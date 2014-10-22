8e0cd05457b8aa1c0badd328b08c744b3e17e117
fishermanswharff <fishermanswharff@github.com>
2014-10-22 02:57:12 -0400

    refactoring, refining, scaling. NYT feeds urls now parsed from nyt website

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,8 +2,12 @@ class Menu
 
   require "#{Rails.root}/app/modules/RssGetter.rb"
 
-  def initialize(url)
-    @hash = RssGetter::NPR.new(url).to_hash
+  def initialize(url, options={})
+    if options.values[0] == "NPR"
+      @hash = RssGetter::NPR.new(url).to_hash
+    elsif options.values[0] == "NYT"
+      @hash = RssGetter::NYT.new(url).to_hash
+    end
   end
 
 end
--- app/modules/RssGetter.rb
+++ app/modules/RssGetter.rb
@@ -32,6 +32,25 @@ module RssGetter
     end
   end
 
+  class NYT
+
+    def initialize(url)
+      @page = Nokogiri::HTML(open(url))
+      parse
+    end
+
+    def parse
+      @rss_links = @page.css("ul.rssColumns a").select { |link| link }
+      @titles = @rss_links.map { |link| link.children.to_s }
+      @urls = @rss_links.map { |link| link['href'] }
+    end
+
+    def to_hash
+      Hash[@titles.zip(@urls.map{|i| i.split /,/})]
+    end
+
+  end
+
 end
 
 # RSSGetter::NPR.new("http://www.npr.org/rss/")
