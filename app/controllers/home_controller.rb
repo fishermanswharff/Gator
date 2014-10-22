2785177c3291dd703f2366869e1e942797e6ffa6
fishermanswharff <fishermanswharff@github.com>
2014-10-22 13:16:59 -0400

    styling up the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,8 @@
 class HomeController < ApplicationController
-
-
   def index
     if User.all.length == 0
       redirect_to new_user_registration_path
     end
-
     if user_signed_in?
       @user = User.find(current_user)
       @user_feeds = Feed.get_user_feeds(current_user)
