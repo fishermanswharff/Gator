2a6bdccfb10fbdf64ef381f94d7f0fb9a9a28bfa
fishermanswharff <fishermanswharff@github.com>
2014-10-22 21:51:46 -0400

    fixing bugs on heroku

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,7 +1,6 @@
 class Menu 
 
   require "#{Rails.root}/app/modules/RssGetter.rb"
-
   def initialize(url, options={})
     if options.values[0] == "NPR"
       @hash = RssGetter::NPR.new(url).to_hash
@@ -9,8 +8,8 @@ class Menu
       @hash = RssGetter::NYT.new(url).to_hash
     elsif options.values[0] == "TMZ"
       @hash = RssGetter::TMZ.new(url).to_hash
-    else
-      @hash = RssGetter::MISC.new(url, options.values[0]).to_hash
+    elsif options.values[0] == "MISC"
+      @hash = RssGetter::Links.new(url, options.values[1]).to_hash
     end
   end
 end
