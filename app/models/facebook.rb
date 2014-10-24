367b066ce50c773796c785e76af3705e556d7f96
fishermanswharff <fishermanswharff@github.com>
2014-10-24 16:04:23 -0400

    cleaning up for best practices

--- app/models/facebook.rb
+++ app/models/facebook.rb
@@ -5,19 +5,19 @@ class Facebook
   format :json
   base_uri 'https://graph.facebook.com/v2.1/'
 
-  def initialize(access_token)
-    @access_token = access_token
-  end
+  # def initialize(access_token)
+  #   @access_token = access_token
+  # end
 
-  def get_info
-    self.class.get('/me',:query => { :access_token => @access_token })
-  end
+  # def get_info
+  #   self.class.get('/me',:query => { :access_token => @access_token })
+  # end
 
-  def get_timeline
-    self.class.get('/me/feed', :query => { :access_token => @access_token } )
-  end
+  # def get_timeline
+  #   self.class.get('/me/feed', :query => { :access_token => @access_token } )
+  # end
 
-  def get_name
-    self.class.get('/me?fields=name', :query => { :access_token => @access_token } )
-  end
+  # def get_name
+  #   self.class.get('/me?fields=name', :query => { :access_token => @access_token } )
+  # end
 end
